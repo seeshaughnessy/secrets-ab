9ff904dac7982df68dc5c265195b684ccb1e59d8
Chris Shaughnessy <chris.shaughnessy@cis-us.com>
2020-07-07 16:38:27 -0600

    Level 3 - Hashing Passwords via md5

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,10 +20,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = "Thisisourlittlesecret";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,7 +38,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(err => {
@@ -55,7 +52,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
