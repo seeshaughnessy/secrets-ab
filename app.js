dbd3e145888b72c2c34b0c66190583d26bbed33b
Chris Shaughnessy <chris.shaughnessy@cis-us.com>
2020-07-07 14:38:00 -0600

    Level 2 - Database Encryption via mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -18,6 +19,10 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+const secret = "Thisisourlittlesecret";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
